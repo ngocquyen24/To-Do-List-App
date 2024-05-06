40336b9be92d1b85e5fa2996ef811bab05188574
ngocquyen <ngocquyencv2468@gmail.com>
2024-05-06 09:12:08 +0700

    setup login

--- Todolist-app/database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ Todolist-app/database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -8,21 +8,25 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email',100)->primary();
-            $table->string('token',100);
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('password_resets');
     }
 };
