c92283ad234edbdc05298db7be0acf8233add969
Thanhdat328 <92036923+Thanhdat328@users.noreply.github.com>
2024-05-30 09:37:19 +0700

    dasboard_user

---
+++ Todolist-app/resources/views/tasks/index.blade.php
@@ -0,0 +1,72 @@
+@extends('dashboard')
+
+@section('content')
+
+<div class="container">
+    <a href="{{ route('tasks.create') }}" class="flex items-center bg-gray-200 px-2 py-1 rounded hover:bg-gray-100 border-b-4 border-gray-400">
+        <span>New Task</span>
+    </a>
+    <table>
+        <tr>
+            <th>#</th>
+            <th>Check</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Action</th>
+        </tr>
+
+        @if($admin_role == 1)
+            @foreach($tasks_admin as $task)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>
+                    <form class="flex items-center gap-2" method="POST" action="{{route('mark')}}">
+                        @csrf
+                        <input class="bg-gray-400 text-green-500" type="checkbox" name="id" value="{{$task->id}}" onClick="this.form.submit()" {{$task->task_status ? 'checked' : ''}}>
+
+                        <input type="hidden" name="id" value="{{$task->id}}" />
+                    </form>
+                    </td>
+                    <td>{{ $task->task_name }}</td>
+                    <td>{{ $task->task_details }}</td>
+                    <td>{{ $task->status }}</td>
+                    <td>
+                        <a href="{{ route('tasks.edit', $task->id) }}" class="hover:text-gray-400">edit</a>
+                    </td>
+                </tr>
+            @endforeach
+        @else
+            @foreach($tasks as $task)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>
+                    <form class="flex items-center gap-2" method="POST" action="{{route('mark')}}">
+                        @csrf
+                        <input class="bg-gray-400 text-green-500" type="checkbox" name="id" value="{{$task->id}}" onClick="this.form.submit()" {{$task->task_status ? 'checked' : ''}}>
+
+                        <input type="hidden" name="id" value="{{$task->id}}" />
+                    </form>
+                    </td>
+                    <td>{{ $task->task_name }}</td>
+                    <td>{{ $task->task_details }}</td>
+                    <td>{{ $task->status }}</td>
+                    <td class='d-flex ps-1 pe-1'>
+                        <a href="{{ route('tasks.edit', $task->id) }}" class="hover:text-gray-400 ms-2 me-2">edit</a>
+                        <form action="{{ route('tasks.destroy', $task->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button onclick="return confirm('Are you sure you want to delete this task?')">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        @endif
+    </table>
+    <a href="{{ route('home') }}">back</a>
+    
+</div>
+
+@endsection
---
+++ Todolist-app/resources/views/user/edit.blade.php
@@ -0,0 +1,40 @@
+@extends('dashboard')
+
+@section('content')
+<div style="width: 480px;"class="max-w-full mx-auto mt-10 px-4 py-8 text-gray-500">
+        <h1 class="text-xl uppercase text-gray-500 mb-4">Update a Task</h1>
+        <form action="{{route('user.update', $user->id)}}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <label class="block" for="task_name"> Name: </label>
+            <input class="w-full mb-2 bg-gray-200" type="text" name="name" value="{{$user->name}}">
+            @error('name')
+                <p class="text-base pb-4 text-red-400">{{$message}}</p>
+            @enderror
+            <label class="block" for="task_details">email: </label>
+            <input class="w-full mb-2  bg-gray-200" type="text" name="email" value="{{$user->email}}">
+            @error('email')
+                <p class="text-base pb-4 text-red-400">{{$message}}</p>
+            @enderror
+            <label class="block" for="task_details">phone: </label>
+            <input class="w-full mb-2  bg-gray-200" type="text" name="phone" value="{{$user->phone}}">
+            @error('phone')
+                <p class="text-base pb-4 text-red-400">{{$message}}</p>
+            @enderror
+            <label class="block" for="task_details">image: </label>
+            <img src="{{asset('avatars/'.$user->avatar.'')}}" alt="">
+            <input class="w-full mb-2  bg-gray-200" type="file" name="avatar">
+            @error('avatar')
+                <p class="text-base pb-4 text-red-400">{{$message}}</p>
+            @enderror
+
+
+
+
+            <div class="flex gap-4">
+                <button class="block bg-gray-400 py-2 px-4 rounded-lg text-gray-100 hover:text-gray-500 focus:outline-none">Update</button>
+                <a href="{{route('user.index')}}" class="block bg-red-500 py-2 px-4 rounded-lg text-gray-100 hover:bg-red-400 focus:outline-none">Cancel</a>
+            </div>
+        </form>
+    </div>
+@endsection
